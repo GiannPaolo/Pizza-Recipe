51116aa98823312664c73dfb0f07e245e0f3d8d6
Giann <122035615+GiannPaolo@users.noreply.github.com>
2024-04-28 23:49:18 +0800

    button functions
    added button functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+function show() {
+  const showingredients = document.querySelector(".marg-ingre");
+  showingredients.style.display = "inline";
+  const menu = document.getElementById("showbutton");
+  menu.style.display = "none";
+  const show = document.getElementById("hide");
+  show.style.display = "inline";
+}
+function hide() {
+  const showingredients = document.querySelector(".marg-ingre");
+  showingredients.style.display = "none";
+  const menu = document.getElementById("showbutton");
+  menu.style.display = "inline";
+  const show = document.getElementById("hide");
+  show.style.display = "none";
+}
